d628aa6c8b8a85dfff1c19c976abffbb2ac9ec0a
BalajiKamalesan96 <balajikamalesan96@gmail.com>
2016-10-18 12:02:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-app.get('/ui/pic.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'pic.jpg'));
-});
 
 app.get('/hello1.html', function (req, res) {
   res.send("ITS UNDER CONSTRUCTION");
